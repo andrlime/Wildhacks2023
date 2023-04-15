a191a04502b9631e745140ab9b3a857a7d87f499
Jerry Han <109703519+jerry-hannn@users.noreply.github.com>
2023-04-15 14:39:05 -0500

    Add Context

--- frontend/src/Login.tsx
+++ frontend/src/Login.tsx
@@ -1,5 +1,5 @@
 import { MantineProvider, Autocomplete, Loader, TextInput, PasswordInput, Button } from '@mantine/core'; // Mantine is a React UI library from https://github.com/rtivital
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState, useEffect, useRef, createContext} from 'react';
 import './index.css';
 import { auth } from './firebase/firebase-config';
 import { createUserWithEmailAndPassword, onAuthStateChanged, updateProfile } from 'firebase/auth';
