cf26fcbe8d20778a4d64931f1a0be866bbeec429
Jerry Han <109703519+jerry-hannn@users.noreply.github.com>
2023-04-15 12:05:47 -0500

    Create firebase-config.js

---
+++ frontend/src/firebase/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDExpOmBvAlncR8T7xJ2iUAfl_Iy0xfwug",
+  authDomain: "wildhacks-2023.firebaseapp.com",
+  projectId: "wildhacks-2023",
+  storageBucket: "wildhacks-2023.appspot.com",
+  messagingSenderId: "1092878813994",
+  appId: "1:1092878813994:web:7cb4e3d016c015e6e8ac53"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
