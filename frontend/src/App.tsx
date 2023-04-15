aab183afba312be5b17990c4f7e17498140906e7
Jerry Han <109703519+jerry-hannn@users.noreply.github.com>
2023-04-15 13:36:24 -0500

    password fields

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { MantineProvider, Autocomplete, Loader } from '@mantine/core'; // Mantine is a React UI library from https://github.com/rtivital
+import { MantineProvider, Autocomplete, Loader, TextInput, PasswordInput } from '@mantine/core'; // Mantine is a React UI library from https://github.com/rtivital
 import React, {useState, useEffect, useRef} from 'react';
 import './index.css';
 import { auth } from '../../assets/firebase/firebase-config';
@@ -6,14 +6,17 @@ import { createUserWithEmailAndPassword, onAuthStateChanged, updateProfile } fro
 
 export const App: React.FC = () => {
   const timeoutRef = useRef<number>(-1);
-  const [value, setValue] = useState('');
+  const [email, setEmail] = useState('');
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<string[]>([]);
+  const [password, setPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   //This code is from https://github.com/rtivital, the developer of Mantine
   const handleChange = (val: string) => {
     window.clearTimeout(timeoutRef.current);
-    setValue(val);
+    setEmail(val);
     setData([]);
 
     if (val.trim().length === 0 || val.includes('@')) {
@@ -30,12 +33,22 @@ export const App: React.FC = () => {
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <Autocomplete
       style={{width: 350,}}
-      value={value}
+      value={email}
       data={data}
       onChange={handleChange}
       rightSection={loading ? <Loader size="1rem" /> : null}
       label="Email"
       placeholder="Your email"
+      />
+      <PasswordInput
+      style={{width: 350,}}
+      label="Password"
+      placeholder="Your password"
+      />
+      <PasswordInput
+      style={{width: 350,}}
+      label="Confirm Password"
+      placeholder="Confirm your password"
       />
       
     </MantineProvider>
