5086853de65285e4c979638e633b8ef812633edb
Andrew Li <54134293+andrlime@users.noreply.github.com>
2023-04-15 21:36:49 -0500

    add some hard coded data

---
+++ frontend/src/Components/data.ts
@@ -0,0 +1,29 @@
+export const subjects = [
+    {value: "JOUR", label: "Journalism"},
+    {value: "CS", label: "Computer Science"},
+    {value: "CE", label: "Computer Engineering"},
+    {value: "EE", label: "Electrical Engineering"},
+    {value: "EA", label: "Engineering Analysis"},
+    {value: "PHYS", label: "Biology"},
+    {value: "CHEM", label: "Chemistry"},
+    {value: "BIO", label: "Biology"}
+];
+
+export const buildings = [
+    {value: "norris", label: "Norris University Center"},
+    {value: "fisk", label: "Fisk Hall"},
+    {value: "locy", label: "Locy Hall"},
+    {value: "harris", label: "Harris Hall"},
+    {value: "univ", label: "University Hall"},
+    {value: "lunt", label: "Lunt Hall"},
+    {value: "allencenter", label: "Allen Center"},
+    {value: "pancoe", label: "Pancoe Life Sciences Pavilion"},
+    {value: "tech", label: "Technological Institute"},
+    {value: "annenberg", label: "Annenberg Hall"},
+    {value: "cahn", label: "Cahn Auditorium"},
+    {value: "searle", label: "Searle Hall"},
+    {value: "ford", label: "Ford Design Center"},
+    {value: "mudd", label: "Mudd Librari"},
+    {value: "kresge", label: "Kresge Hall"},
+    {value: "swift", label: "Swift Hall"},
+];
