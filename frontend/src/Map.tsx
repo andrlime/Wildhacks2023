4aadba43583b989064aceb72a12d1bf2c3424212
Andrew Li <54134293+andrlime@users.noreply.github.com>
2023-04-15 22:00:12 -0500

    update map:

--- frontend/src/Map.tsx
+++ frontend/src/Map.tsx
@@ -1,8 +1,15 @@
 import './leaflet.css';
-import { MapContainer, TileLayer, useMap , Popup, Marker} from 'react-leaflet'
+import { MapContainer, TileLayer, useMap, Popup, Marker} from 'react-leaflet';
+import L from "leaflet";
+import DEMO_IMAGE from './Components/demo.png';
 
-export default function Map()
-{
+export default function Map() {
+    const myIcon = L.icon({
+        iconUrl: DEMO_IMAGE,
+        iconSize: [40, 40],
+        iconAnchor: [20, 20],
+    });
+
     return (
         <div>
             <MapContainer className="map-container" center={[42.056, -87.675]} zoom={16} scrollWheelZoom={false}>
@@ -10,7 +17,7 @@ export default function Map()
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[42.056, -87.675]}>
+            <Marker position={[42.056, -87.675]} icon={myIcon}>
                 <Popup>
                 CS 211. <br /> 1 person is studying here.
                 </Popup>
