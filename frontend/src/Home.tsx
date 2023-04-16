4ecaf946206e400290b1e19855799e4fa98cb4e1
Jerry Han <109703519+jerry-hannn@users.noreply.github.com>
2023-04-15 20:51:32 -0500

    Create Modal for Profile

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,14 +1,16 @@
-import { MantineProvider, Autocomplete, Loader, TextInput, PasswordInput, Button } from '@mantine/core'; // Mantine is a React UI library from https://github.com/rtivital
+import { MantineProvider, Modal, Group, TextInput, PasswordInput, Button } from '@mantine/core'; // Mantine is a React UI library from https://github.com/rtivital
 import React, {useState, useEffect, useRef} from 'react';
 import './index.css';
 import { auth } from './firebase/firebase-config';
 import { createUserWithEmailAndPassword, onAuthStateChanged, updateProfile } from 'firebase/auth';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 import { redirect, useNavigate } from 'react-router-dom';
+import { useDisclosure } from '@mantine/hooks';
 
 
 export const Home: React.FC = () => {
 
+    const [opened, { open, close }] = useDisclosure(false);
     const navigate = useNavigate();
     const signOut = () => {
         auth.signOut();
@@ -17,15 +19,18 @@ export const Home: React.FC = () => {
 
     return (
         <MantineProvider theme={{ colorScheme: 'light' }}>
+            <Modal opened={opened} onClose={close} title="Profile" centered>
+                {/*Modal Content*/}
+            </Modal>
             <Button
                 style={{width: "fit-content",}}
                 className='bg-purple-500 hover:bg-purple-700'
                 variant='filled'
                 color="violet"
                 title='Sign Out'
-                onClick={signOut}
+                onClick={open}
                 >
-                Sign Out
+                Profile
             </Button>
         </MantineProvider>
     );
