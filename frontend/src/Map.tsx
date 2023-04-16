7de33c6818a620a292821313530b2cd631bd0cd1
ElysiaL528 <elysialopez528@gmail.com>
2023-04-15 20:08:08 -0500

    Modified code to use components from React Leaflet library

--- frontend/src/Map.tsx
+++ frontend/src/Map.tsx
@@ -1,49 +1,21 @@
-import {useState, useEffect, useRef} from 'react';
 import './leaflet.css';
-
-import L, { LeafletMouseEvent } from 'leaflet';
+import { MapContainer, TileLayer, useMap , Popup, Marker} from 'react-leaflet'
 
 export default function Map()
 {
-  const [didSetMap, setDidSetMap] = useState(false);
-
-    useEffect(() => {
-
-        const script = document.createElement('script');
-      
-        script.src = "https://unpkg.com/leaflet@1.9.3/dist/leaflet.js";
-        script.async = true;
-      
-        document.body.appendChild(script);
-
-        if(didSetMap === false)
-        {
-          console.log("rice krispy");
-
-          let map = L.map('leafletmap').setView([42.056, -87.675], 16);
-
-          L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-          map.on('click', function(e) {mapClicked(e)});
-
-          setDidSetMap(true);
-        }
-      
-        return () => {
-          document.body.removeChild(script);
-        }
-      }, [didSetMap]);
-
     return (
-        <div className="map-container" id="leafletmap">
+        <div>
+            <MapContainer className="map-container" center={[42.056, -87.675]} zoom={16} scrollWheelZoom={false}>
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[42.056, -87.675]}>
+                <Popup>
+                CS 211. <br /> 1 person is studying here.
+                </Popup>
+            </Marker>
+            </MapContainer>
         </div>
     );
-}
-
-function mapClicked(e : LeafletMouseEvent)
-{
-  let latlng = e.latlng;
-  console.log("you clicked: " + latlng);
 }
