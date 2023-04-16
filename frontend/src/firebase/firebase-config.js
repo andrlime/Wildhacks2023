49ab97a807e8669fe03f7064c81f32e2e1d4232b
Jerry Han <109703519+jerry-hannn@users.noreply.github.com>
2023-04-15 19:04:22 -0500

    Update firebase-config.js

--- frontend/src/firebase/firebase-config.js
+++ frontend/src/firebase/firebase-config.js
@@ -12,9 +12,11 @@ const firebaseConfig = {
   projectId: "wildhacks-2023",
   storageBucket: "wildhacks-2023.appspot.com",
   messagingSenderId: "1092878813994",
-  appId: "1:1092878813994:web:7cb4e3d016c015e6e8ac53"
+  appId: "1:1092878813994:web:7cb4e3d016c015e6e8ac53",
+  databaseURL: "https://wildhacks-2023-default-rtdb.firebaseio.com/"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getDatabase(app);
